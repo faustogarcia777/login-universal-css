4a7721f58bd0893565f4f644ac05640164823ef2
faustogarcia777 <fausto.garcia@coppel.com>
2025-12-05 11:00:28 -0700

    Create login-id-mobile-social.js

---
+++ login-id-mobile-social.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("mobile-social-container").innerHTML = `
+    <div class="mobile-social-container">
+      <div class="mobile-social-buttons">
+
+        <button class="mobile-social-button" onclick='socialButtonTag("Passkey"),loginWithSocial("passkey")'>
+          <img class="mobile-social-icon" src="https://faustogarcia777.github.io/login-universal-css/face-id.svg" alt="Passkey">
+        </button>
+
+        <button class="mobile-social-button" onclick='socialButtonTag("Google"),loginWithSocial("google")'>
+          <img class="mobile-social-icon" src="https://cdn5.coppel.com/Auth0/SVG/google-icon.svg" alt="Google">
+        </button>
+
+        <button class="mobile-social-button" onclick='socialButtonTag("Microsoft"),loginWithSocial("windowslive")'>
+          <img class="mobile-social-icon" src="https://cdn5.coppel.com/Auth0/SVG/microsoft-icon.svg" alt="Microsoft">
+        </button>
+
+        <button class="mobile-social-button" onclick='socialButtonTag("Apple"),loginWithSocial("apple")'>
+          <img class="mobile-social-icon" src="https://cdn5.coppel.com/Auth0/SVG/apple-icon.svg" alt="Apple">
+        </button>
+
+      </div>
+    </div>
+  `;
+});
